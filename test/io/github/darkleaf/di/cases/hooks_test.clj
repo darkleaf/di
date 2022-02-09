55ab4360351729be0cfee9d7c5376fb90c80b622
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2022-02-09 19:56:49 +0400

    logging-hook test

--- test/io/github/darkleaf/di/cases/hooks_test.clj
+++ test/io/github/darkleaf/di/cases/hooks_test.clj
@@ -9,3 +9,29 @@
         b    (fn [ident obj] (conj obj ::b))
         hook (di/join-hooks a b)]
     (t/is (= [::a ::b] (hook ::obj obj)))))
+
+
+(defn logging-hook [logger]
+  (fn [ident object]
+    (if (fn? object)
+      (fn [& args]
+        (let [result (apply object args)]
+          (swap! logger conj [ident args result])
+          result))
+      object)))
+
+(defn service-a [{} arg]
+  [::a arg])
+
+(defn service-b [{a `service-a} arg1 arg2]
+  [::b (a arg1) (a arg2)])
+
+(t/deftest logging-hook-test
+  (let [logger (atom [])]
+    (with-open [b (di/start `service-b {} (logging-hook logger))]
+      (t/is (= [::b [::a 1] [::a 2]]
+               (b 1 2))))
+    (t/is (= [[`service-a [1] [::a 1]]
+              [`service-a [2] [::a 2]]
+              [`service-b [1 2] [::b [::a 1] [::a 2]]]]
+             @logger))))
