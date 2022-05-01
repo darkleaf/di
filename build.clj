b6ac398271c962575cb745fcb4d9859ea89aef95
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2022-05-01 23:43:54 +0400

    scm-url && 1.0.1

--- build.clj
+++ build.clj
@@ -1,11 +1,24 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]))
+  (:require
+   [clojure.tools.build.api :as b]
+   [clojure.string :as str]))
 
 (def lib 'org.clojars.darkleaf/di)
-(def version "1.0.0")
+(def version "1.0.1")
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s.jar" (name lib)))
+(def scm-url "git@github.com:darkleaf/di.git")
+
+(defn sha
+  [{:keys [dir path] :or {dir "."}}]
+  (-> {:command-args (cond-> ["git" "rev-parse" "HEAD"]
+                       path (conj "--" path))
+       :dir (.getPath (b/resolve-path dir))
+       :out :capture}
+      b/process
+      :out
+      str/trim))
 
 (defn clean [_]
   (b/delete {:path "target"}))
@@ -15,7 +28,11 @@
                 :lib       lib
                 :version   version
                 :basis     basis
-                :src-dirs  ["src"]})
+                :src-dirs  ["src"]
+                :scm       {:tag                 (sha nil)
+                            :connection          (str "scm:git:" scm-url)
+                            :developerConnection (str "scm:git:" scm-url)
+                            :url                 scm-url}})
   (b/copy-dir {:src-dirs   ["src" "resources"]
                :target-dir class-dir})
   (b/jar {:class-dir class-dir
