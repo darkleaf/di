19af3c1b5f2fd855a345ced484e7ec170b6148d9
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2022-09-08 22:32:50 +0400

    ref-missed-test

--- test/darkleaf/di/ref_test.clj
+++ test/darkleaf/di/ref_test.clj
@@ -1,13 +1,21 @@
 (ns darkleaf.di.ref-test
   (:require
    [clojure.test :as t]
-   [darkleaf.di.core :as di]))
+   [darkleaf.di.core :as di])
+  (:import
+   (clojure.lang ExceptionInfo)))
 
 (t/deftest ref-test
   (with-open [obj (di/start `object
                             {`object     (di/ref `replacement)
                              `replacement ::stub})]
     (t/is (= ::stub @obj))))
+
+(t/deftest ref-missed-test
+  (t/is (thrown-with-msg? ExceptionInfo
+                          #"\AMissing dependency darkleaf.di.ref-test/dep\z"
+                          (di/start ::root
+                                    {::root (di/ref `dep)}))))
 
 (t/deftest pr-test
   (t/is (= "#darkleaf.di.core/ref darkleaf.di.ref-test/object"
