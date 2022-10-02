26bce6cad5b2b56a83e1ddb22caff7e35fbde7c1
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2022-10-02 19:12:33 +0400

    add-side-dependency test

---
+++ test/darkleaf/di/tutorial/y_add_side_dependency_test.clj
@@ -0,0 +1,18 @@
+(ns darkleaf.di.tutorial.y-add-side-dependency-test
+  (:require
+   [clojure.test :as t]
+   [darkleaf.di.core :as di]))
+
+(defn root []
+  'root)
+
+(defn migrations [{::keys [*migrated?]}]
+  (deliver *migrated? true))
+
+(t/deftest add-side-dependency-test
+  (let [*migrated? (promise)]
+    (with-open [root (di/start `root
+                               (di/add-side-dependency `migrations)
+                               {::*migrated? *migrated?})]
+      (t/is (= 'root @root)))
+    (t/is (deref *migrated? 0 false))))
