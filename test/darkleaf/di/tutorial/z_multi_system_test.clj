bb356d1e281dde6ab3d86fd0ec1ce5c194a3ce7f
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2022-10-15 21:53:00 +0400

    multi system

--- test/darkleaf/di/tutorial/z_multi_system_test.clj
+++ test/darkleaf/di/tutorial/z_multi_system_test.clj
@@ -3,16 +3,19 @@
    [clojure.test :as t]
    [darkleaf.di.core :as di]))
 
-(defn shared [{}]
-  (Math/random))
+(defn shared []
+  (Object.))
 
-(defn server [{shared `shared}]
-  shared)
+(defn server [{name   ::name
+               shared `shared}]
+  [name shared])
 
 (t/deftest multi-system-test
   (with-open [shared (di/start `shared)
-              a      (di/start `server {`shared @shared})
-              b      (di/start `server {`shared @shared})]
-    (t/is (double? @a))
-    (t/is (double? @b))
-    (t/is (identical? @a @b))))
+              a      (di/start `server {`shared @shared
+                                        ::name  :a})
+              b      (di/start `server {`shared @shared
+                                        ::name  :b})]
+    (t/is (= :a (first @a)))
+    (t/is (= :b (first @b)))
+    (t/is (identical? (second @a) (second @b)))))
