f475fc1846c58a0e2ddbf482df1d52c8c72424e0
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2022-10-15 23:05:01 +0400

    deps

--- test/darkleaf/di/tutorial/b_dependencies_test.clj
+++ test/darkleaf/di/tutorial/b_dependencies_test.clj
@@ -5,12 +5,18 @@
   (:import
    (clojure.lang ExceptionInfo)))
 
-;; DI uses map destructuring syntax to describe dependencies of a component.
+;; DI uses associative destructuring syntax to define dependencies of a component.
 ;; https://clojure.org/guides/destructuring#_associative_destructuring
 
-;; зависимости объявляются ключами. Ключи - это символы, кейворды и строки.
+;; There is a mapping between keys and components.
+;; A key can be symbol, keyword, or string.
+;; In this chapter we'll use only symbols.
 
-;; todo: как-то тут рассказать про все типы ключей
+;; If we use symbols DI will try to resolve a component's var.
+
+;; In the following example the `root` component depends on
+;; the `a` and `b` and the `b` is optional.
+;; You also can get all component dependencies by the `deps` binding.
 
 (defn root [{a      `a
              ::syms [b]
@@ -24,14 +30,17 @@
   (with-open [root (di/start `root)]
     (t/is (= [:root ::a ::default {`a ::a}] @root))))
 
-;; позже рассмотрим реестры
+;; `di/start` can accepts additional arguments.
+;; In the following example the argument is a map registry.
+;; I use it to define local keys.
+;; In general they are middlewares but I'll describe it later.
 
 (t/deftest root-with-extra-deps-test
   (with-open [root (di/start `root {`b ::b})]
     (t/is (= [:root ::a ::b {`a ::a `b ::b}] @root))))
 
-
-;; Dependencies are required by default
+;; Dependencies are required by default.
+;; There is no defenition of `a'` so DI will throw an exception.
 
 (defn root' [{a `a'}]
   [::root a])
