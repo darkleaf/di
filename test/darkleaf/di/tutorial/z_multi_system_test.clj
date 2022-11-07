0d5e529abd79c5dc6113b52e85aecd9f65734bb8
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2022-11-07 21:44:10 +0400

    multi system

--- test/darkleaf/di/tutorial/z_multi_system_test.clj
+++ test/darkleaf/di/tutorial/z_multi_system_test.clj
@@ -3,6 +3,12 @@
    [clojure.test :as t]
    [darkleaf.di.core :as di]))
 
+;; In some cases, you may need multiple systems and sharing a subsystem between them.
+;; In that case just pass specify the subsystem in registry.
+
+;; To get a value of the subsystem, you should `deref` it as you would for a regular system root.
+;; Also you should manually stop systems in reverse order.
+
 (defn shared []
   (Object.))
 
@@ -19,6 +25,3 @@
     (t/is (= :a (first @a)))
     (t/is (= :b (first @b)))
     (t/is (identical? (second @a) (second @b)))))
-
-
-;; TODO: you should stop systems  самостоятельно
