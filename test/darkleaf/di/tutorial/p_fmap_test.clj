c1329fe977a07893a1d3d776ec143245d5870ba3
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2023-01-20 21:10:35 +0400

    fmap test

--- test/darkleaf/di/tutorial/p_fmap_test.clj
+++ test/darkleaf/di/tutorial/p_fmap_test.clj
@@ -36,19 +36,3 @@
 (t/deftest box-test
   (with-open [root (di/start `box {::b :b})]
     (t/is (= [:b] @root))))
-
-;; todo: rewrite
-(t/deftest stoppable-test
-  (let [*log (atom [])]
-    (with-open [root (di/start ::root {::root (di/fmap (reify dip/Factory
-                                                         (dependencies [_])
-                                                         (build [_ _]
-                                                           (reify dip/Stoppable
-                                                             (unwrap [_]
-                                                               :a)
-                                                             (stop [_]
-                                                               (swap! *log conj :a)))))
-                                                       (fn [a]
-                                                         [:fmap a]))})]
-      (t/is (= [:fmap :a] @root)))
-    (t/is (= [:a] @*log))))
