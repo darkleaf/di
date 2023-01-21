cfdf68226c58c3bb2703c4886287cf191c780042
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2023-01-21 22:04:20 +0400

    update example

--- example/src/example/adapters/hikari.clj
+++ example/src/example/adapters/hikari.clj
@@ -1,15 +1,9 @@
 (ns example.adapters.hikari
   (:require
    [hikari-cp.core :as hikari]
-   [darkleaf.di.core :as di]
-   [darkleaf.di.protocols :as di.p])
-  (:import
-   (com.zaxxer.hikari HikariDataSource)))
+   [darkleaf.di.core :as-alias di]))
 
-(defn datasource [{options ::options}]
+(defn datasource
+  {::di/stop hikari/close-datasource}
+  [{options ::options}]
   (hikari/make-datasource options))
-
-(extend-type HikariDataSource
-  di.p/Stoppable
-  (stop [this]
-    (hikari/close-datasource this)))
--- example/src/example/adapters/jetty.clj
+++ example/src/example/adapters/jetty.clj
@@ -1,16 +1,10 @@
 (ns example.adapters.jetty
   (:require
-   [darkleaf.di.core :as di]
-   [darkleaf.di.protocols :as di.p]
-   [ring.adapter.jetty :as jetty])
-  (:import
-   [org.eclipse.jetty.server Server]))
+   [darkleaf.di.core :as-alias di]
+   [ring.adapter.jetty :as jetty]))
 
-(defn server [{handler ::handler
-               options ::options}]
+(defn server
+  {::di/stop #(.stop %)}
+  [{handler ::handler
+    options ::options}]
   (jetty/run-jetty handler (assoc options :join? false)))
-
-(extend-type Server
-  di.p/Stoppable
-  (stop [this]
-    (.stop this)))
