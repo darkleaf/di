187481fc806e62384689ce3ed4c32c171dbe0d82
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2023-03-31 20:14:33 +0400

    verbose test for starting-many-keys-test

--- test/darkleaf/di/tutorial/q_starting_many_keys_test.clj
+++ test/darkleaf/di/tutorial/q_starting_many_keys_test.clj
@@ -6,6 +6,12 @@
 (def a :a)
 (def b :b)
 
+(t/deftest verbose-test
+  (with-open [root (di/start ::root {::root (di/template [(di/ref `a) (di/ref `b)])})]
+    (let [[a b] @root]
+      (t/is (= :a a))
+      (t/is (= :b b)))))
+
 (t/deftest ok-test
   (with-open [root (di/start [`a `b])]
     (let [[a b] root]
