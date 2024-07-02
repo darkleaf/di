2067e751b5be356f4e8e2aa4fa08b358b42ea8bc
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2024-07-02 21:19:55 +0400

    update example

--- example/src/example/adapters/flyway.clj
+++ example/src/example/adapters/flyway.clj
@@ -1,10 +1,13 @@
 (ns example.adapters.flyway
   (:require
+   [darkleaf.di.core :as-alias di]
    [example.adapters.hikari :as-alias hikari])
   (:import
    (org.flywaydb.core Flyway)))
 
-(defn migrate [{ds `hikari/datasource}]
+(defn migrate
+  {::di/kind :component}
+  [{ds `hikari/datasource}]
   (.. (Flyway/configure)
       (dataSource ds)
       load
