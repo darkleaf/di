4a035209a33b580c256a9f272954117c9678e34a
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2024-09-28 21:08:45 +0400

    update comparison of Integrant

--- notebooks/integrant.clj
+++ notebooks/integrant.clj
@@ -1,5 +1,9 @@
-;; # Comparison of Integrant and DI
+;; # Comparing Integrant and DI for Dependency Injection in Clojure
+;;
+;; This article compares two approaches to dependency injection in Clojure: [Integrant](https://github.com/weavejester/integrant) and [DI](https://github.com/darkleaf/di).
+;; We'll explore their differences through code examples and discuss the pros and cons of each approach.
 
+```clojure
 (ns integrant
   {:nextjournal.clerk/toc true}
   (:require
@@ -9,25 +13,34 @@
 {:nextjournal.clerk/visibility {:result :hide}}
 
 ;; ## Assumptions
-;; Here I'm writing code in a single file and
-;; I have to use short namespaces like `:jetty/server` instead of `::jetty/server`.
 ;;
-;; Here I'm not using real dependencies to keep it short.
-;; Please see [the example app](https://github.com/darkleaf/di/tree/master/example).
+;; - All code is written in a single file.
+;; - Short namespaces like `:jetty/server` are used instead of `::jetty/server`.
+;; - Real dependencies are omitted for brevity. For a complete example, see [the example app](https://github.com/darkleaf/di/tree/master/example).
+;;
+;; ## Code Examples
+;;
+;; First, we'll define a fake generic `stop` function to simulate stopping a component:
 
-;; ## Code
 (defn stop
   "Fake generic stop function"
-  [x])
+  [x]
+  ;; Placeholder for actual stop logic
+  ,)
 
-;; ### Jetty
+;; ### Jetty Server
+;;
+;; #### Using Integrant
 
 (defmethod ig/init-key :jetty/server [_ {:keys [handler port]}]
+  ;; In a real application, you might use:
   ;; (jetty/run-jetty handler {:port port, :join? false})
   [:jetty handler port])
 
 (defmethod ig/halt-key! :jetty/server [_ server]
   (stop server))
+
+;; #### Using DI
 
 (defn jetty-server
   {::di/stop #(stop %)}
@@ -35,60 +48,83 @@
     port    "PORT"}]
   [:jetty handler port])
 
-;; With DI, use symbols for var names,
-;; keywords for abstract dependencies to define them later,
-;; and strings for environment variables.
-
+;; With DI:
+;;
+;; - Use **symbols** for var names.
+;; - Use **keywords** for abstract dependencies to define them later.
+;; - Use **strings** for environment variables.
+;;
 ;; ### Routes
+;;
+;; #### Using Integrant
 
 (defmethod ig/init-key :web/route-data [_ {:keys [root-handler]}]
   [["/" {:get {:handler root-handler}}]])
+
+;; #### Using DI
 
 (def route-data
   (di/template
    [["/" {:get {:handler (di/ref `root-handler)}}]]))
 
-;; ``(di/ref `root-handler)`` resolves to `root-handler` var.
-;; There is no need to define root-handler as a component in the system config.
-
+;; Here, `(di/ref `root-handler)` resolves to the `root-handler` var.
+;; There's no need to define `root-handler` as a component in the system config.
+;;
 ;; ### Reitit
+;;
+;; We need to transform `route-data` into a Ring handler.
 
 (defn route-data->handler [route-data]
-  #_(-> route-data
-        (ring/router)
-        (ring/ring-handler))
+  ;; In a real application, you might use:
+  ;; (-> route-data
+  ;;     (ring/router)
+  ;;     (ring/ring-handler))
   :ring-handler)
 
+;; #### Using Integrant
+
 (defmethod ig/init-key :web/handler [_ {:keys [route-data]}]
-   (route-data->handler route-data))
+  (route-data->handler route-data))
+
+;; #### Using DI
 
 (def web-handler (di/derive `route-data route-data->handler))
 
-;; You can also use these variants as well:
+;; Alternatively:
+;;
 ;; ```clojure
 ;; (defn web-handler [{route-data `route-data}]
 ;;   (route-data->handler route-data))
+;; ```
 ;;
+;; Or:
+;;
+;; ```clojure
 ;; (defn web-handler [{:syms [route-data]}]
 ;;   (route-data->handler route-data))
 ;; ```
-
+;;
 ;; ### Handlers
+;;
+;; #### Using Integrant
 
 (defmethod ig/init-key :web/root-handler [_ {:keys []}]
   (fn [req]
     :ok))
 
+;; #### Using DI
+
 (defn root-handler [-deps req]
   :ok)
 
-;; Instead of Integrant, with DI you don't have to restart the system while
-;; developing `root-handler`. Just eval defn form again.
-
-;; With Integrant, you have to use complicated `ig/suspend-key!` and `ig/resume-key`
-;; to preserve the state of the system.
-
-;; ### System
+;; With DI, you don't need to restart the system while developing `root-handler`.
+;; Just re-evaluate the `defn` form.
+;; With Integrant, you'd need to use `ig/suspend-key!` and `ig/resume-key`
+;; to preserve the system state.
+;;
+;; ### System Initialization
+;;
+;; #### Using Integrant
 
 (def ig-config
   {:jetty/server     {:port    8080
@@ -99,40 +135,40 @@
 
 (ig/init ig-config)
 
+;; #### Using DI
+;;
+
 (di/start `jetty-server {"PORT"         8080
                          :jetty/handler (di/ref `web-handler)})
 
-;; `:jetty/handler` is an abstraction.
-;; I don't need to depend on a concrete key in the namespace with the jetty component.
-
-;; With DI, you only need to declare abstract dependencies in the registry.
-;; Also, with the registry, you can override any key.
-
-;; ## Real applications
-
-;; That was a trivial example.
-;; In my application I'm using two different databases and may add a third.
-
-;; > Vassal of my vassal is not my vassal
+;; In DI:
 ;;
-;; I have layers in my application.
-;; I think, my components should declare dependencies by themselves.
-;; My handler depends on database connection and auth token decoder.
-;; And the decoder depends on secret key as an environment variable.
-
-;; With Integrant or Component you have options for how to link handlers and
-;; stateful components:
-;; 1. Make every stateless handler a component.
-;; And you have a lot of configuration.
-;; 2. [Pass](https://github.com/prestancedesign/usermanager-reitit-example/blob/main/src/usermanager/controllers/user.clj#L73)
-;; all stateful components (except Jetty) with a ring request.
-
-;; With DI you have a better solution.
-;; Don't touch anything except the handler, just define a new dependency:
-
-(comment
-  (defn root-handler [-deps req]
-    :ok))
+;; - `:jetty/handler` is an abstraction.
+;; - You don't need to depend on a concrete key in the namespace with
+;;   the Jetty component.
+;; - Only declare abstract dependencies in the registry.
+;; - The registry allows you to override any key.
+;;
+;; ## Real Applications
+;;
+;; In a real application, you might have multiple databases and layers.
+;;
+;; > The vassal of my vassal is not my vassal.
+;;
+;; Components should declare their dependencies themselves.
+;; For example, a handler depends on a database connection and an auth token decoder,
+;; and the decoder depends on a secret key from an environment variable.
+;;
+;; ### Handling Dependencies
+;;
+;; With Integrant or Component, you have options for linking handlers and stateful components:
+;;
+;; 1. **Make every stateless handler a component.**
+;; - Results in a lot of configuration.
+;; 2. **Pass stateful components via the Ring request.**
+;; - [Example](https://github.com/prestancedesign/usermanager-reitit-example/blob/main/src/usermanager/controllers/user.clj#L73).
+;;
+;; With DI, you can define dependencies directly:
 
 (defn root-handler* [{db      :db/datasource
                       decoder `token-decoder}
@@ -142,24 +178,27 @@
 (defn token-decoder [{key "SECRET_KEY"} token]
   :decoded)
 
-;; Allowing you to easily define as many stateless components as you need is the main goal of DI.
-
+;; This allows you to define as many stateless components as needed,
+;; which is the main goal of DI.
+;;
 ;; ## Subsystems
+;;
+;; In a web application with independent subsystems
+;; (e.g., `/subsystem-a`, `/subsystem-b`),
+;; each subsystem has its own `route-data` and uses common components.
+;;
+;; ### Using DI to Extend Routes
+;;
+;; With `di/update-key`,
+;; you can extend base route-data with specific one in each subsystem.
 
-;; In my web application I have independent subsustems like
-;; * /subsystem-a
-;; * /subsystem-b
-;; * /etc
-
-
-;; Each subsystem has its own route-data and uses only common components.
-
-;; With `di/update-key` I extend base route-data with specific one in each subsystem.
-
-(def subsystem-a-route-data [["/subsystem-a/" '...]])
+(def subsystem-a-route-data
+  [["/subsystem-a/" '...]])
 
 (defn subsystem-a []
   (di/update-key `route-data concat `subsystem-a-route-data))
+
+;; Starting the system:
 
 (di/start `jetty-server
           {"PORT"         8080
@@ -167,13 +206,13 @@
           (subsystem-a)
           #_(subsystem-N))
 
-;; ## Feature flags
-
+;; ## Feature Flags
+;;
 ;; In most cases I prefer to use feature flags instead of branching.
-;; DI allows it:
+;; It can be implemented easily with DI:
 
-(defn subsystem-a* [{:keys [subsytem-a-enabled]}]
-  (if subsytem-a-enabled
+(defn subsystem-a* [{:keys [subsystem-a-enabled]}]
+  (when subsystem-a-enabled
     (di/update-key `route-data concat `subsystem-a-route-data)))
 
 (defn registry [flags]
@@ -184,25 +223,31 @@
 
 (di/start `jetty-server (registry {:subsystem-a-enabled true}))
 
-;; ## Error handling
-
-;; Both Integrant and Component break REPL development.
-;; What happens if `ig/init-key` throws an exception?
-;; You have started the Jetty server and it is listening on port.
-;; You have to restart the REPL to continue development.
-
-;; DI is smart enough to stop already started components.
-
-;; UPD: You can use [integrant-repl](https://github.com/weavejester/integrant-repl).
-;; It [takes care](https://github.com/weavejester/integrant-repl/blob/master/src/integrant/repl.clj#L22) of this case.
-
-;; ## AOP
-
-;; I think AOP is quite dangerous practice.
-;; But some times I need to add extra behavior to my existing component without touching it.
-;; Like logging, performance measurement, spec checking.
-;; With Integrant, there is no convenient way to update an existing component.
-;; You have to [rename](https://github.com/weavejester/integrant/issues/58)
-;; the key of you component, and reconfigure all dependent components.
-
-;; With DI, you could use `di/update-key` and `di/instrument`.
+;; ## Error Handling
+;;
+;; Both Integrant and Component can hinder REPL development
+;; if `ig/init-key` throws an exception.
+;; For example, if the Jetty server starts and listens on a port
+;; but initialization fails, you might need to restart the REPL.
+;;
+;; DI is smart enough to stop already started components in such cases.
+;;
+;; *Note*: You can use [integrant-repl](https://github.com/weavejester/integrant-repl), which handles this scenario by [stopping components on failure](https://github.com/weavejester/integrant-repl/blob/master/src/integrant/repl.clj#L22).
+;;
+;; ## Aspect-Oriented Programming (AOP)
+;;
+;; While AOP can be a dangerous practice,
+;; sometimes you need to add extra behavior to existing components
+;; (e.g., logging, performance measurement, spec checking).
+;;
+;; With Integrant, there's no convenient way to update an existing component.
+;; You'd need to [rename the component key](https://github.com/weavejester/integrant/issues/58) and reconfigure dependent components.
+;;
+;; With DI, you can use `di/update-key` and `di/instrument` to modify components.
+;;
+;; ---
+;;
+;; This comparison highlights how DI provides a flexible and dynamic approach
+;; to dependency injection in Clojure,
+;; especially in terms of REPL-driven development and managing complex dependencies.
+;; Integrant, while powerful, may require more boilerplate during development.
