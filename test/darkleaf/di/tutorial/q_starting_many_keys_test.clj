9b36a264c135c9693525c525ca89d49e9c987ec7
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2024-10-15 21:45:44 +0400

    Add di/with-open

--- test/darkleaf/di/tutorial/q_starting_many_keys_test.clj
+++ test/darkleaf/di/tutorial/q_starting_many_keys_test.clj
@@ -7,29 +7,29 @@
    [darkleaf.di.core :as di]
    [clojure.test :as t]))
 
+;; The standard `with-open` does not support destructuring in bindings.
+;; Use `di/with-open` to handle resources with destructuring support.
+
 (def a :a)
 (def b :b)
 
 (t/deftest verbose-test
-  (with-open [root (di/start ::root {::root (di/template [(di/ref `a) (di/ref `b)])})]
-    (let [[a b] @root]
-      (t/is (= :a a))
-      (t/is (= :b b)))))
+  (di/with-open [[a b] (di/start ::root {::root (di/template [(di/ref `a) (di/ref `b)])})]
+    (t/is (= :a a))
+    (t/is (= :b b))))
 
 ;; The root container implements `clojure.lang.Indexed`
 ;; so you can use destructuring without derefing the root.
 
 (t/deftest indexed-test
-  (with-open [root (di/start [`a `b])]
-    (let [[a b] root]
-      (t/is (= :a a))
-      (t/is (= :b b)))))
+  (di/with-open [[a b] (di/start [`a `b])]
+    (t/is (= :a a))
+    (t/is (= :b b))))
 
 ;; The root container implements `clojure.lang.ILookup`
 ;; so you can use destructuring without derefing the root.
 
 (t/deftest lookup-test
-  (with-open [root (di/start {:a `a :b `b})]
-    (let [{:keys [a b]} root]
-      (t/is (= :a a))
-      (t/is (= :b b)))))
+  (di/with-open [{:keys [a b]} (di/start {:a `a :b `b})]
+    (t/is (= :a a))
+    (t/is (= :b b))))
