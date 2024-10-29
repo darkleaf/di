a235a7d1c6948d77b97957ea6d093449a60b5bee
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2024-10-29 16:53:05 +0400

    fix indent

--- src/darkleaf/di/core.clj
+++ src/darkleaf/di/core.clj
@@ -278,79 +278,79 @@
           ctx         {:registry   registry
                        :*stop-list (volatile! '())}
           obj         (try-build ctx key)]
-     ^{:type   ::root
-       ::print obj}
-     (reify
-       AutoCloseable
-       (close [_]
-         (->> (try-stop-started ctx)
-              (throw-many!)))
-       IDeref
-       (deref [_]
-         obj)
-       Indexed
-       (nth [_    i]
-         (nth obj i))
-       (nth [_    i not-found]
-         (nth obj i not-found))
-       (count [_]
-         (count obj))
-       ILookup
-       (valAt [_  key]
-         (get obj key))
-       (valAt [_  key not-found]
-         (get obj key not-found))
-       IFn
-       (call [_]
-         (.call ^IFn obj))
-       (run [_]
-         (.run ^IFn obj))
-       (invoke [this]
-         (.invoke ^IFn obj))
-       (invoke [_          a1]
-         (.invoke ^IFn obj a1))
-       (invoke [_          a1 a2]
-         (.invoke ^IFn obj a1 a2))
-       (invoke [_          a1 a2 a3]
-         (.invoke ^IFn obj a1 a2 a3))
-       (invoke [_          a1 a2 a3 a4]
-         (.invoke ^IFn obj a1 a2 a3 a4))
-       (invoke [_          a1 a2 a3 a4 a5]
-         (.invoke ^IFn obj a1 a2 a3 a4 a5))
-       (invoke [_          a1 a2 a3 a4 a5 a6]
-         (.invoke ^IFn obj a1 a2 a3 a4 a5 a6))
-       (invoke [_          a1 a2 a3 a4 a5 a6 a7]
-         (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7))
-       (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8]
-         (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8))
-       (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9]
-         (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9))
-       (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10]
-         (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10))
-       (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11]
-         (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11))
-       (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12]
-         (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12))
-       (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13]
-         (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13))
-       (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14]
-         (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14))
-       (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15]
-         (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15))
-       (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16]
-         (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16))
-       (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17]
-         (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17))
-       (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18]
-         (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18))
-       (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19]
-         (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19))
-       (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20]
-         (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20))
-       (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 args]
-         (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 args))
-       (applyTo [_ args]
-         (.applyTo ^IFn obj args))))))
+      ^{:type   ::root
+        ::print obj}
+      (reify
+        AutoCloseable
+        (close [_]
+          (->> (try-stop-started ctx)
+               (throw-many!)))
+        IDeref
+        (deref [_]
+          obj)
+        Indexed
+        (nth [_    i]
+          (nth obj i))
+        (nth [_    i not-found]
+          (nth obj i not-found))
+        (count [_]
+          (count obj))
+        ILookup
+        (valAt [_  key]
+          (get obj key))
+        (valAt [_  key not-found]
+          (get obj key not-found))
+        IFn
+        (call [_]
+          (.call ^IFn obj))
+        (run [_]
+          (.run ^IFn obj))
+        (invoke [this]
+          (.invoke ^IFn obj))
+        (invoke [_          a1]
+          (.invoke ^IFn obj a1))
+        (invoke [_          a1 a2]
+          (.invoke ^IFn obj a1 a2))
+        (invoke [_          a1 a2 a3]
+          (.invoke ^IFn obj a1 a2 a3))
+        (invoke [_          a1 a2 a3 a4]
+          (.invoke ^IFn obj a1 a2 a3 a4))
+        (invoke [_          a1 a2 a3 a4 a5]
+          (.invoke ^IFn obj a1 a2 a3 a4 a5))
+        (invoke [_          a1 a2 a3 a4 a5 a6]
+          (.invoke ^IFn obj a1 a2 a3 a4 a5 a6))
+        (invoke [_          a1 a2 a3 a4 a5 a6 a7]
+          (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7))
+        (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8]
+          (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8))
+        (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9]
+          (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9))
+        (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10]
+          (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10))
+        (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11]
+          (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11))
+        (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12]
+          (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12))
+        (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13]
+          (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13))
+        (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14]
+          (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14))
+        (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15]
+          (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15))
+        (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16]
+          (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16))
+        (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17]
+          (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17))
+        (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18]
+          (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18))
+        (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19]
+          (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19))
+        (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20]
+          (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20))
+        (invoke [_          a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 args]
+          (.invoke ^IFn obj a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 args))
+        (applyTo [_ args]
+          (.applyTo ^IFn obj args))))))
 
 (defn stop
   "Stops the root of a system"
