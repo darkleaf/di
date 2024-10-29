c6202cd95eeb7906c3938ea5af3a849961b2b2ca
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2024-10-29 20:28:45 +0400

    update key test

--- test/darkleaf/di/update_key_test.clj
+++ test/darkleaf/di/update_key_test.clj
@@ -12,3 +12,12 @@
          (di/start ::component
                    {::component 1}
                    (di/update-key ::component-with-typo inc)))))
+
+(t/deftest bug2--update-non-existent-key-test
+  (t/is (thrown-with-msg?
+         ExceptionInfo
+         #"\ACan't update non-existent key :darkleaf.di.update-key-test/component-with-typo\z"
+         (di/start ::component
+                   {::component          1
+                    ::not-used-component 2}
+                   (di/update-key ::not-used-component inc)))))
