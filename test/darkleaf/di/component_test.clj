7735ee6b3f6101e22d38fc5789bf77e697bb7cfc
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2024-12-22 20:18:05 +0400

    component test

--- test/darkleaf/di/component_test.clj
+++ test/darkleaf/di/component_test.clj
@@ -5,30 +5,29 @@
   (:import
    (clojure.lang ExceptionInfo)))
 
-(defmacro catch-cause-data [& body]
-  `(try ~@body
-        (catch clojure.lang.ExceptionInfo e#
-          (->> e#
-               Throwable->map
-               :via
-               (mapv :data)))))
+(defmacro try-catch [& body]
+  `(try
+     ~@body
+     nil
+     (catch Exception ex#
+       ex#)))
 
-(defn nil-value-component
+(defn nil-value-component-0-arity
   {::di/kind :component}
   []
   nil)
 
 (defn nil-value-component-1-arity
   {::di/kind :component}
-  [_]
+  [-deps]
   nil)
 
-(t/deftest nil-value-component-test
-  (t/is (= [{:type  ::di/build-obj-fail
-             :stack [`nil-value-component ::di/implicit-root]}
-            {:type ::di/nil-value-component}]
-           (catch-cause-data (di/start `nil-value-component))))
-  (t/is (= [{:type  ::di/build-obj-fail
-             :stack [`nil-value-component-1-arity ::di/implicit-root]}
-            {:type ::di/nil-value-component}]
-           (catch-cause-data (di/start `nil-value-component-1-arity)))))
+(t/deftest nil-value-component-0-arity-test
+  (let [ex (try-catch (di/start `nil-value-component-0-arity))]
+    (t/is (= "An error during component build" (-> ex ex-message)))
+    (t/is (= "A component fn can't return nil" (-> ex ex-cause ex-message)))))
+
+(t/deftest nil-value-component-1-arity-test
+  (let [ex (try-catch (di/start `nil-value-component-1-arity))]
+    (t/is (= "An error during component build" (-> ex ex-message)))
+    (t/is (= "A component fn can't return nil" (-> ex ex-cause ex-message)))))
