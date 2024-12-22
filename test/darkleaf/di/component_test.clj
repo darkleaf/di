53e5d4e25d803fbb1bc22ab1c0ec8d278831c9a6
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2024-12-22 20:39:20 +0400

    fix test

--- test/darkleaf/di/component_test.clj
+++ test/darkleaf/di/component_test.clj
@@ -25,9 +25,9 @@
 (t/deftest nil-component-0-arity-test
   (let [ex (try-catch (di/start `nil-component-0-arity))]
     (t/is (= "An error during component build" (-> ex ex-message)))
-    (t/is (= "A component fn can't return nil" (-> ex ex-cause ex-message)))))
+    (t/is (= "A component fn should not return nil" (-> ex ex-cause ex-message)))))
 
 (t/deftest nil-component-1-arity-test
   (let [ex (try-catch (di/start `nil-component-1-arity))]
     (t/is (= "An error during component build" (-> ex ex-message)))
-    (t/is (= "A component fn can't return nil" (-> ex ex-cause ex-message)))))
+    (t/is (= "A component fn should not return nil" (-> ex ex-cause ex-message)))))
