2efa566df693333d79fe8b7b59d0759908c21fd9
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2024-12-30 17:47:40 +0400

    use catch-some

--- test/darkleaf/di/component_test.clj
+++ test/darkleaf/di/component_test.clj
@@ -1,16 +1,8 @@
 (ns darkleaf.di.component-test
   (:require
    [clojure.test :as t]
-   [darkleaf.di.core :as di])
-  (:import
-   (clojure.lang ExceptionInfo)))
-
-(defmacro try-catch [& body]
-  `(try
-     ~@body
-     nil
-     (catch Exception ex#
-       ex#)))
+   [darkleaf.di.core :as di]
+   [darkleaf.di.test-utils :refer [catch-some]]))
 
 (defn nil-component-0-arity
   {::di/kind :component}
@@ -23,11 +15,9 @@
   nil)
 
 (t/deftest nil-component-0-arity-test
-  (let [ex (try-catch (di/start `nil-component-0-arity))]
-    (t/is (= "An error during component build" (-> ex ex-message)))
+  (let [ex (catch-some (di/start `nil-component-0-arity))]
     (t/is (= "A component fn should not return nil" (-> ex ex-cause ex-message)))))
 
 (t/deftest nil-component-1-arity-test
-  (let [ex (try-catch (di/start `nil-component-1-arity))]
-    (t/is (= "An error during component build" (-> ex ex-message)))
+  (let [ex (catch-some (di/start `nil-component-1-arity))]
     (t/is (= "A component fn should not return nil" (-> ex ex-cause ex-message)))))
